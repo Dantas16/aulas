43915242e8d097b37bca3f4ae0d4313199501689
Gutemberg Dantas <gutembergdantas16@hotmail.com>
2022-05-23 20:05:07 -0300

    Aula dia 23 de Maio de 2022

--- ejs/index.js
+++ ejs/index.js
@@ -5,10 +5,26 @@ const port = 3000;
 app.set('view engine', 'ejs');
 //app.set('views', './views');
 
-app.get('/', (req, resp) => {
-  resp.render('pages/home')
+let menu = [
+  { rota: '/', titulo: 'Home' },
+  { rota: '/sobre', titulo: 'Sobre' }
+]
+
+app.get('/', (request, response) => {
+  let titulo = "Bem vindo a Escola Feliz";
+
+  response.render('pages/home', { titulo, menu })
 })
 
+app.get('/sobre', (request, response) => {
+  let titulo = "Escola Feliz - Sobre nós";
+
+  response.render('pages/sobre', { titulo, menu })
+})
+
+app.get('/alunos', (request, response) => {
+  response.render('pages/alunos')
+})
 
 app.listen(port, () => {
   console.log('Express started at: ' + port);
