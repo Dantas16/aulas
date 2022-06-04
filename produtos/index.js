be46d0d09fb32b64a888dfe2882cc370b55346d7
Gutemberg Dantas <gutembergdantas16@hotmail.com>
2022-06-04 00:33:29 -0300

    Aula 03 de Junho de 2022

--- produtos/index.js
+++ produtos/index.js
@@ -1,20 +1,25 @@
-const express = require('express');
+const express = require('express')
 const app = express();
-const port = 3200;
+const port = 3500;
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (request, response) => {
+app.get("/produtos",(request, response)=> {
     console.log('Requisicao recebida');
+ var listaProdutos =[ 
+     {id:0, name:"café"},
+     {id:1, name:"agua"},
+     {id:2, name:"arroz"},
+    ]
 
-    return response.send('Bem vindo(a)');
+    return response.send(listaProdutos);
 })
 
 app.get('/home', (request, response) => {
     response.render('index');
-});
+})
 
 app.listen(port, () => {
-    console.log('Serviço na porta: ' + port);
+console.log('Serviço na porta: '+ port)
 })
--- produtos/public/indexFront.js
+++ produtos/public/indexFront.js
@@ -1,12 +1,21 @@
-
 function helloWorld() {
-    fetch('http://localhost:3200')
-        .then(resposta => console.log(resposta) )
-        .catch(error => console.error(error) )
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Pagina carregada completamente');
-
-    helloWorld();
-})
+    console.log("hello wolrd")
+        fetch ('http://localhost:3500/produtos')
+        .then(resp => resp.json())
+        .then( resposta =>{
+            try{
+                var produtos = resposta
+                console.log(produtos)
+            } catch(maria){
+                console.error(maria.message)
+            }
+        } )
+        .catch(error => console.log(error))
+        console.log("fim")
+    }
+    
+    document.addEventListener('DOMContentLoaded',function() {
+        console.log("Pagina carregada corretamente");
+        helloWorld()
+    })
+    
