b302af27867a0021ddad766a561581845cf21d40
Gutemberg Dantas de Oliveira Alencar <gutemberg.alencar@serpro.gov.br>
2022-09-30 18:38:08 -0300

    aula 30 de setembro de 2022

--- login/index.js
+++ login/index.js
@@ -7,23 +7,25 @@ app.use(express.json());
 app.post('/login', async (req, resp) => {
     const { email, password } = req.body;
 
-    if (email == 'joao@gmail.com' && password == '12345678') {
         //TODO verificar se email e senha corretos e retornar para o cliente com o ID
         const { conectar } = require('./config/db');
-        const sequelizeConexao = conectar();
+        const sequelizeConexao = await conectar();
 
         const { Usuario } = require('./models/usuario');
         const usuario = await Usuario(sequelizeConexao).findOne({
+            attributes: ["id", "nome"],
             where: {
                 email,
                 senha: password
             }
         });
+    
+    if (usuario) { 
 
-        resp.send(usuario);
+        resp.status(200).send({ usuario });
     } else {
 
-        resp.send({ message: 'Dados inválidos' });
+        resp.status(401).send({ message: 'Dados inválidos' });
     }
 })
 
